b090d8eb6fa19bfc5842454ebcb397f49a99e7f1
Thu Trang Vu <vuthutrang2811@gmail.com>
2025-01-28 18:25:54 +0200

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, FlatList, TouchableOpacity, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const [item, setItem] = useState('');
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const addItem = () => {
+    if (item.trim() !== '') {
+      setShoppingList([...shoppingList, { key: item }]);
+      setItem('');
+    }
+  };
+
+  const clearShoppingList = () => {
+    setShoppingList([]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Todo List</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Insert item"
+        value={item}
+        onChangeText={setItem}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={addItem}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.clearButton]} onPress={clearShoppingList}>
+          <Text style={styles.buttonText}>Clear</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.title}>Shopping List</Text>
+      <FlatList
+        ListEmptyComponent={
+          <View style={{ alignItems: 'center', marginTop: 20 }}>
+            <Text>No data</Text>
+          </View>
+        }
+        data={shoppingList}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Text>{item.key}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
+    margin : 30
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginTop: 40,
+    marginBottom: 20,
+    borderRadius: 5,
+    fontSize: 18
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  button: {
+    flex: 1,
+    backgroundColor: '#007BFF',
+    padding: 15,
     alignItems: 'center',
-    justifyContent: 'center',
+    borderRadius: 5,
+    margin :20
+    
+  },
+  clearButton: {
+    backgroundColor: '#ff4d4d',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  listItem: {
+    padding: 10,
+    alignItems : 'center',
+    fontSize: 18
   },
 });
